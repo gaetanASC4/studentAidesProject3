ec9eaf72dd158d4431f3ddac2d3a49714baf5fdd
gaetan <stuie25138@gmail.com>
2018-02-20 18:44:02 -0500

    done

--- ans.sql
+++ ans.sql
@@ -1,14 +1,61 @@
 -- JUST FYI, THE SCHEMA IS
 -- vending_machine (id INTEGER PRIMARY KEY, name VARCHAR, amount INTEGER, type CHAR, cost FLOAT)
 
--- Show all of the rows of data
+--Show all of the rows of data
 
--- Show only rows that have a type of “candy”
+SELECT * FROM vending_machine;
 
--- Show all rows in descending order by name
+--Show only rows that have a type of “candy”
+
+SELECT * FROM vending_machine WHERE type = "candy";
+
+--Show all rows in descending order by name
+
+SELECT * FROM vending_machine ORDER BY name ASC;
 
 -- Show only rows that have a type of “liquid” AND have an amount under 5
 
+SELECT * FROM vending_machine WHERE type = "liquid" AND amount < 5;
+
 -- Show only rows that have a type of “snack” in ascending order by cost
 
+SELECT * FROM vending_machine WHERE type = "snack" ORDER BY cost ASC;
+
 -- BONUS: Create a new table named “snacks” with the result of the last command!
+
+DROP TABLE IF EXISTS snacks;
+CREATE TABLE snacks 
+(
+    id INTEGER PRIMARY KEY, 
+    name VARCHAR, 
+    amount INTEGER,
+    type CHAR,
+    cost FLOAT
+);
+
+INSERT INTO snacks SELECT * FROM vending_machine WHERE type = "snack" ORDER BY cost ASC;
+
+-- Show the name and quantity of the least expensive item
+
+SELECT * FROM vending_machine WHERE cost = (SELECT MIN(cost) FROM vending_machine);
+
+-- Show the count of different items (basically the total number of rows)
+
+SELECT COUNT(*) FROM vending_machine;
+
+-- Show the average cost of an item as “average cost”
+
+SELECT AVG(cost) FROM vending_machine;
+
+-- Show the name and total cost of each item. The total cost = quantity of the item * cost
+
+SELECT amount * cost FROM vending_machine;
+
+-- Show all items grouped by type
+
+SELECT *
+FROM vending_machine
+ORDER BY type;
+
+-- BONUS: Show the total cost of all items in the vending machine (sum of (each item’s cost * quantity of the item)) as “total cost”
+
--- students_seed.sql
+++ students_seed.sql
@@ -1,5 +1,4 @@
 -- If a database named “students” already exists, delete it
-
 -- Create a database named “students” with the following schema:
 --      id (should be the PRIMARY KEY when making new row)
 --      name (example: “Mahdi Shadkamfarrokhi”)
@@ -8,13 +7,35 @@
 --      grade (example: 12)
 --      fav_hobby (example: “coding”)
 
--- Add the following students and their information to the “students” database:
---      Bob Bobbington/15/Goldman Sachs/11/eating
---      Juan Pablo/17/JPMC/12/gaming
---      Jerry Mathews/16/Bond Collective/11/coding
---      Anthony Quinn/15/Yahoo/11/bowling
---      Kevin Calderon/17/Bond Collective/12/styling
+-- CREATE TABLE students(
+--     studentID INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER,
+--     cohort TEXT,
+--     grade INTEGER,
+--     fav_hobby TEXT
+-- );
+-- Commented out the create table because it was trying to re-create the table every time
+-- --INSERT INTO students 
+-- (name, age, cohort, grade, fav_hobby)
+-- VALUES
+-- ("Bob Bobbington", 15, "Goldman Sachs", 11, "eating"),
+-- ("Juan Pablo", 17, "JPMC", 12, "gaming"),
+-- ("Jerry Mathews", 16, "Bond Collective", 11, "coding"),
+-- ("Anthony Quinn", 15, "Yahoo", 11, "bowling"),
+-- ("Kevin Calderon", 17, "Bond Collective", 12, "styling");
 
+UPDATE students 
+SET cohort = "Facebook"
+WHERE studentID = 4;
+
+UPDATE students 
+SET age = 16    
+WHERE studentID = 2; 
+
+UPDATE students 
+SET fav_hobby = "stuntin"    
+WHERE studentID = 5; 
 -- Make the following modifications (as a separate command) to the “students” database:
 --      Change Anthony Quinn’s cohort to “Facebook”
 --      Change Juan Pablo’s age to 16
